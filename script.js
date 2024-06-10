7b826edf1a4882240d30d351381325a4f11f5448
Felipe <126893383+feoliveira7@users.noreply.github.com>
2024-06-10 16:23:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function apertar(){
+   
+
+    Swal.fire({
+        title: "Parabéns!",
+        text: "Você ganhou um prêmio no valor de R$30.000 MIL REAIS!",
+        icon: "success"
+      });
+
+}
+
+function apertar2(){
+   
+
+    Swal.fire({
+        title: "Parabéns!",
+        text: "Você ganhou um curso completo de matemática!",
+        icon: "success"
+      });
+}
+
+function apertar3(){
+   
+
+    Swal.fire({
+        title: "Parabéns!",
+        text: "Você ganhou um cupom de 60% de desconto no nosso App!",
+        icon: "success"
+      });
+}
